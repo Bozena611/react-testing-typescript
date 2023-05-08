a94cb0befd504979711f5a2dacc798c2bb3a2f9e
Bozena11 <bozena.vuckovic@gmail.com>
2023-05-08 20:00:05 +0200

    Add .spec test and __tests__ folder

---
+++ src/__tests__/Greet-test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import { Greet } from '../components/greet/Greet';
+
+test('Greet renders correctly', () => {
+	render(<Greet />)
+	/* const textElement = screen.getByText('Hello')*/
+	const textElement = screen.getByText(/hello/i) /*using regex instead of the string argument to ignore the letter case*/
+	expect(textElement).toBeInTheDocument()
+})
