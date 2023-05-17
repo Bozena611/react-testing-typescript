d56f164054d1f93b056d1777a38d9d815f729636
Bozena11 <bozena.vuckovic@gmail.com>
2023-05-17 14:39:38 +0200

    Add two ways to debug the code

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, logRoles } from "@testing-library/react";
 import { Skills } from "./Skills";
 
 
@@ -34,7 +34,9 @@ describe("Skills", () => {
 	});
 
 	test("Start Learning button is eventually displayed", async () => {
-		render(<Skills skills={skills} />)
+		const view = render(<Skills skills={skills} />)
+		logRoles(view.container)
+		// screen.debug()
 		const startLearningButton = await screen.findByRole("button", {
 			name: "Start learning",
 		},
@@ -42,6 +44,7 @@ describe("Skills", () => {
 			timeout: 2000,
 		}
 		);
+		// screen.debug()
 		expect(startLearningButton).toBeInTheDocument();
 	});
 });
